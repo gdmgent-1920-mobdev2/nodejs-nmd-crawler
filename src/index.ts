5ba70fa47917fcb2ec44baa19acc495e60def284
drdynscript <drdynscript@gmail.com>
2020-03-05 10:03:06 +0100

    Add routes

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,13 @@ const PORT: Number = Number(process.env.PORT) || 8080;
 // Create Express server
 const app: Express = express();
 
+// Routes
+app.get('/nmd', (req: express.Request, res: express.Response) => {
+    res.status(200).json({
+        message: "Greathings Earthling we are NMD, you will be assimilated!",
+        createdAt: Date.now()
+    });
+});
+
 // Run the Server on a specified port
 app.listen(PORT, () => console.log(`Server running and listining on, port ${PORT}`));
